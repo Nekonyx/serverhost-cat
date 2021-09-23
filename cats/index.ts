32f7e6f8573ddde0d3ce19bb21e381e7d2e1a075
Nekonyx <nekonyx@protonmail.com>
2021-09-24 01:11:46 +0600

    zirix.cat & baguete.cat

--- cats/index.ts
+++ cats/index.ts
@@ -1,6 +1,8 @@
+import baguete from './images/baguete.gif'
 import hiViper from './images/hi-viper.gif'
 import quincy from './images/quincy.gif'
+import zirix from './images/zirix.gif'
 
-const cats = [quincy, hiViper]
+const cats = [quincy, hiViper, baguete, zirix]
 
 export default cats
