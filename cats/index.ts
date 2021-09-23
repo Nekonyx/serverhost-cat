7c8eb58ac80d2cfa3505d3b9da2329fb08c5eaf8
Nekonyx <nekonyx@protonmail.com>
2021-09-24 01:14:46 +0600

    lfs.cat & gunTears.cat & business.cat

--- cats/index.ts
+++ cats/index.ts
@@ -1,8 +1,10 @@
 import baguete from './images/baguete.gif'
+import business from './images/business.gif'
+import gunTears from './images/gunTears.gif'
 import hiViper from './images/hi-viper.gif'
 import quincy from './images/quincy.gif'
 import zirix from './images/zirix.gif'
 
-const cats = [quincy, hiViper, baguete, zirix]
+const cats = [quincy, hiViper, baguete, zirix, gunTears, business]
 
 export default cats
